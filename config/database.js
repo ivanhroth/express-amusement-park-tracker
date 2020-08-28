502eb656549c1afd8a1646cfa0bcdc6acf949424
Wren McPherson <wren.mcpherson@yahoo.com>
2020-08-28 14:02:24 -0400

    Finished phase 5

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 const { db } = require('./index');
-const { username, password, database, host } = db;
-
-module.exports = {
-  development: {
-    username,
-    password,
-    database,
-    host,
-    dialect: 'postgres'
+const { username, password, database, host} = db;
+module.exports = {  
+  development: 
+  {    
+    username,    
+    password,    
+    database,    
+    host,    
+    dialect: 'postgres'  
   }
 };
