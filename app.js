78456b69673b4c79b53490001a1ee8632c7d8bc2
Ivan Roth <ivan.h.roth@gmail.com>
2020-08-28 09:57:15 -0700

    finish phase 2

--- app.js
+++ app.js
@@ -8,4 +8,35 @@ app.set('view engine', 'pug');
 app.use(morgan('dev'));
 app.use('/', router);
 
+const inDevelopment = process.env.NODE_ENV === "development";
+const inProduction = process.env.NODE_ENV === 'production';
+
+app.use((req, res, next) => {
+    let error = new Error("The requested page couldn't be found.");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    if(inDevelopment){
+        console.error(error);
+    }
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    if(error.status === 404) {
+        res.status(404);
+        res.render('page-not-found', { title: 'Page Not Found' });
+    }
+    else next(error);
+});
+
+app.use((error, req, res, next) => {
+    if (error.status) res.status = error.status;
+    else res.status(500);
+    if (!inProduction) res.render('error', { title: "Server Error", message: error.message, stack: error.stack });
+    else res.render('error', { title: "Server Error", message: null, stack: null });
+});
+
 module.exports = app;
--- routes.js
+++ routes.js
@@ -5,5 +5,11 @@ router.get('/', (req, res) => {
     res.render('index', { title: 'Home' })
 })
 
+if (process.env.NODE_ENV !== "production") {
+    router.get("/error-test", () => {
+      throw new Error("This is a test error.");
+    });
+  }
+
 
 module.exports = router;
