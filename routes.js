b188c1b11a6baf283db0973fdc010e695ac38853
Wren McPherson <wren.mcpherson@yahoo.com>
2020-08-28 16:18:03 -0400

    Finished phase 7

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('./db/models');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -9,7 +10,17 @@ if (process.env.NODE_ENV !== "production") {
     router.get("/error-test", () => {
       throw new Error("This is a test error.");
     });
-  }
+}
 
+const asyncHandler = (handler) => {
+    return (req, res, next) => {
+        return handler(req, res, next).catch(next);
+    }
+}
+
+router.get('/parks', asyncHandler(async (req, res) => {
+    let parks = await db.Park.findAll({ order: [['parkName', 'ASC']] });
+    res.render('park-list', { title: 'Parks', parks })
+}))
 
 module.exports = router;
